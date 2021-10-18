aadf13acbdc1fee91e2626f216f1ecbb8a76aef8
royderks <10717410+royderks@users.noreply.github.com>
2021-10-18 22:11:18 +0200

    Added more request methods

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,18 @@
+let names = [{ name: 'John Doe' }]
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      res.status(200).json(names);
+      break;
+    case 'POST':
+      names = [...names, { name: 'Harry Potter'}]
+      res.status(200).json(names);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'PUT']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 }
